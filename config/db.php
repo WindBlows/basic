9a41f6efc25a80165b782886dabfbfc759d1344e
gaoyang <137542696@qq.com>
2017-01-13 10:54:15 +0800

    new

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=shop',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '123456',
     'charset' => 'utf8',
     'tablePrefix' => 'shop_',
 ];
